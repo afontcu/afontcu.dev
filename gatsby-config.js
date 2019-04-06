727cc1e05d496067390c0acc79ae933ed7cfe836
Adrià Fontcuberta <afontcu@gmail.com>
2019-04-06 19:34:57 +0200

    Replace dummy content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Adrià Fontcuberta`,
+    author: `Adrià Fontcuberta`,
+    description: `Personal blog of Adrià Fontcuberta`,
+    siteUrl: `https://afontcu.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `afontcu_`,
     },
   },
   plugins: [
