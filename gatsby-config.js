6812b0cbe28c8b0b779dc16d14d7b343282a6985
Adrià Fontcuberta <afontcu@gmail.com>
2019-05-01 09:22:36 +0200

    Fix manifest data

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Adrià Fontcuberta`,
+        short_name: `afontcu.dev`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#134896`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.jpg`,
       },
     },
     `gatsby-plugin-offline`,
