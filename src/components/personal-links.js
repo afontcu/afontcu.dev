2daccda656c41372d502d739561b5d8ea9e36ee5
Adrià Fontcuberta <adria.fontcuberta@holaluz.com>
2021-12-16 20:53:10 +0100

    Add link to espetech cast

--- src/components/personal-links.js
+++ src/components/personal-links.js
@@ -47,8 +47,8 @@ export default () => (
       href="https://noti.st/afontcu"
     >
       speak here and there
-    </a>{' '}
-    and{' '}
+    </a>
+    ,{' '}
     <a
       target="_blank"
       rel="noopener noreferrer"
@@ -56,6 +56,14 @@ export default () => (
       href="https://buttondown.email/afontcu"
     >
       write a newsletter
+    </a>
+    . Ah! I participo a{' '}
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://anchor.fm/espetech-cast/"
+    >
+      Espetech Cast
     </a>
     .
   </div>
